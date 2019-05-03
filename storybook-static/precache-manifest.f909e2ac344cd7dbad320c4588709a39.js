8ab8a0dba20b3da98e93e36de8006b9b0b9967f8
Nishant S <nishant.s@zemosolabs.com>
2019-05-03 17:39:05 +0530

    taskbox UI static

---
+++ storybook-static/precache-manifest.f909e2ac344cd7dbad320c4588709a39.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea7a7a6ac7ebfa3c258e00527580af5",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "765528bfb4f444cda323",
+    "url": "main.css"
+  },
+  {
+    "url": "main.eb64782acf95e656e6dc.bundle.js"
+  },
+  {
+    "url": "runtime~main.eb64782acf95e656e6dc.bundle.js"
+  },
+  {
+    "url": "vendors~main.eb64782acf95e656e6dc.bundle.js"
+  }
+]);
